83234e6c0b6c0b02d2caedb802467c7e0931c854
Scaelz <avchursins@gmail.com>
2020-02-02 15:27:12 +0300

    Customer_logic
    customer logic prototype

---
+++ Assets/MiscUnitLogic/IMovable.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public interface IMovable
+{
+    float Speed { get; }
+
+    void MoveTo(Vector3 position);
+    void Stop();
+}
---
+++ Assets/UserInput/IUserInput.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class IUserInput 
+{
+    event Action OnButton;
+    event Action OnButtonDown;
+    event Action OnButtonUp;
+}
