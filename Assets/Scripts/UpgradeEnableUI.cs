ce680722439b432cb2c0dbf2a2cd818312045035
mxnoob <noobmx@gmail.com>
2020-02-26 19:01:34 +0300

    менюшка

---
+++ Assets/Scripts/UpgradeEnableUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeEnableUI : MonoBehaviour
+{
+    [SerializeField] GameObject menu;
+    [SerializeField] bool menuEnabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu.SetActive(menuEnabled);
+    }
+    public void OnEnabled()
+    {
+        if (menuEnabled) menuEnabled = false; else menuEnabled = true;
+
+        menu.SetActive(menuEnabled);
+    }
+}
